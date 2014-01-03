1b6469cb0b753355f8a10516f9593e456a59362f
axialraf <rcabezas@axialexchange.com>
2014-01-03 11:38:16 -0500

    Update NSDate+InternetDateTime.m
    Added support for dates without time, as seen in some RSS feeds.

--- Classes/NSDate+InternetDateTime.m
+++ Classes/NSDate+InternetDateTime.m
@@ -75,6 +75,10 @@ static NSDateFormatter *_internetDateTimeFormatter = nil;
                     [dateFormatter setDateFormat:@"EEE, d MMM yyyy HH:mm"]; 
                     date = [dateFormatter dateFromString:RFC822String];
                 }
+                if (!date) { // Sun, 19 May 2002
+                    [dateFormatter setDateFormat:@"EEE, d MMM yyyy"];
+                    date = [dateFormatter dateFromString:RFC822String];
+                }
             } else {
                 if (!date) { // 19 May 2002 15:21:36 GMT
                     [dateFormatter setDateFormat:@"d MMM yyyy HH:mm:ss zzz"]; 
@@ -90,6 +94,10 @@ static NSDateFormatter *_internetDateTimeFormatter = nil;
                 }
                 if (!date) { // 19 May 2002 15:21
                     [dateFormatter setDateFormat:@"d MMM yyyy HH:mm"]; 
+                    date = [dateFormatter dateFromString:RFC822String];
+                }
+                if (!date) { // 19 May 2002
+                    [dateFormatter setDateFormat:@"d MMM yyyy"];
                     date = [dateFormatter dateFromString:RFC822String];
                 }
             }
